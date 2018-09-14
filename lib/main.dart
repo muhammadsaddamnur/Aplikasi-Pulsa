02beb1c9fc8c86b95163410cc3872f15260852c0
MuhammadSaddamNur <muhammadsaddamnur@gmail.com>
2018-09-14 10:06:45 +0700

    udah bisa card baris 3 dengan effect ripple(Inkwell)

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,89 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
 
-void main(){
-  runApp(MaterialApp(
-    home: simpixiedata(),
-  ));
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(new MyApp());
+  });
 }
 
-class simpixiedata extends StatefulWidget{
-  @override
-  simpixiestate createState() => simpixiestate();
-}
-
-class simpixiestate extends State<simpixiedata>{
-  /*final String url = "https://simpixie.com/api/android/Gameidx/slim-api/public/news/";*/
-  final String url = "https://simpixie.com/api/response.php";
-  List data;
-
-  Future<String> getSWData() async {
-    var res = await http.get(Uri.encodeFull(url),headers: {"Accept": "application/json"});
-
-    setState(() {
-      var resBody = json.decode(res.body);
-      data = resBody["message"]; //isi dari datanya message "message":[{//
-    });
-
-    return "Success";
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Simpixie"),
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: ListView.builder(
-        itemCount: data == null ? 0 : data.length,
-        itemBuilder: (BuildContext context, int index){
-          return new Container(
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Card(
-                    child: Container(
-                      padding: EdgeInsets.all(15.0),
-                      child: Row(
-                        children: <Widget>[
-                          Text("Kode :"),
-                          Text(data[index]["code"],
-                            style: TextStyle(
-                                fontSize: 18.0,
-                                color: Colors.black54
-                            ),
-                          ),
-                          Text("Harga :"),
-                          Text(data[index]["price"],
-                            style: TextStyle(
-                                fontSize: 18.0,
-                                color: Colors.black54
-                            ),
-                          ),
-                        ],
-                      )
-
-                    ),
-                  ),
 
 
-
-//                  Card(
-//                    child: Container(
-//                      padding: EdgeInsets.all(15.0),
-//                      child: Text(data[index]["price"],
-//                        style: TextStyle(
-//                            fontSize: 18.0,
-//                            color: Colors.black54
-//                        ),),
-//                    ),
-//                  )
-
-
-
-                ],
+    final crdPulsa = new Container(
+      margin: new EdgeInsets.only(left: 10.0, right: 10.0),
+      child: new SizedBox(
+        height: 80.0,
+        child: new Card(
+            child: new InkWell(
+              onTap: (){print("tapped");},
+              child: new ListTile(
+                leading: new Icon(Icons.phone, color: Colors.lightBlue,size: 26.0,),
+                title: new Text("Pulsa"),
+                subtitle: new Text("Ayo Isi Pulsa Disini Lebih Murah"),
               ),
             ),
-          );
-        },
+        ),
+      ),
+    );
+
+
+    final crdData = new Container(
+      margin: new EdgeInsets.only(left: 10.0, right: 10.0),
+      child: new SizedBox(
+        height: 80.0,
+        child: new Card(
+          child: new InkWell(
+            onTap: (){print("tapped");},
+            child: new ListTile(
+              leading: new Icon(Icons.wifi, color: Colors.lightBlue,size: 26.0,),
+              title: new Text("Paket Data"),
+              subtitle: new Text("Paket Data Lebih Hemat"),
+            ),
+          ),
+        ),
+      ),
+    );
+
+
+    final crdToken = new Container(
+      margin: new EdgeInsets.only(left: 10.0, right: 10.0),
+      child: new SizedBox(
+        height: 80.0,
+        child: new Card(
+          child: new InkWell(
+            onTap: (){print("tapped");},
+            child: new ListTile(
+              leading: new Icon(Icons.battery_charging_full, color: Colors.lightBlue,size: 26.0,),
+              title: new Text("Token Listrik"),
+              subtitle: new Text("Isi Token Listrikmu"),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    return new MaterialApp(
+      title: "",
+      home: new Scaffold(appBar: new AppBar(
+        title: new Text("Hello"),
+      ),
+        body: new Container(
+          margin: new EdgeInsets.only(top: 300.0),
+          child: new Column(
+            children: <Widget>[
+              crdPulsa,
+              crdData,
+              crdToken
+            ],
+          ),
+        ),
       ),
     );
   }
-  @override
-  void initState() {
-    super.initState();
-    this.getSWData();
-  }
+
 }
+
